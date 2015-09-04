e4cd61c3662a114fbe3fcecbffe2aaadb71a6b58
Alex Zorg <azorg@mail.ru>
2015-09-04 16:31:19 +0400

    update

--- vsgettime/_start.sh
+++ vsgettime/_start.sh
@@ -12,10 +12,11 @@ fi
 OPTIM="-O0 -g"
 DEFS="-DVSGETTIME_TEST $DEFS"
 CFLAGS="$DEFS $OPTIM -Wall -pipe"
+LDFLAGS="-lrt"
 GCC="gcc"
 #LD="gcc"
 
-${GCC} ${CFLAGS} vsgettime.c -o test_vsgettime || exit
+${GCC} ${CFLAGS} vsgettime.c -o test_vsgettime ${LDFLAGS} || exit
 
 ./test_vsgettime
 
